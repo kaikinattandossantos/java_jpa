90619886aa603bec2653f53d7b85fac71363fb4b
kaikinattandossantos <kaikinattansg@gmail.com>
2025-08-12 23:26:05 -0300

    first commit

---
+++ src/main/java/com/example/demo/NeurotechApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeurotechApplication.class, args);
+	}
+
+}
